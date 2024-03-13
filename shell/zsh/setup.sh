67ba728c05cd11a39cc06095c5e9f3807257d153
excute <oys1751@gmail.com>
2024-03-13 11:29:18 +0900

    refine zshrc link script

--- shell/zsh/setup.sh
+++ shell/zsh/setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+DATE_STR=$(TZ=UTC-9:00 date "+%Y-%m-%d-%H-%M-%S")
+
+if [ -f $HOME/.zshrc ]; then
+    cp $HOME/.zshrc $HOME/.zshrc_backup_$DATE_STR
+    rm $HOME/.zshrc
+fi
+
 # link zshrc
 ln -s ./.zshrc $HOME/.zshrc
