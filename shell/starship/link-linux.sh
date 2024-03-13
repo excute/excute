4a3f6af578369b8c8b64193f0d4497a580cb169c
excute <oys1751@gmail.com>
2024-03-13 11:14:48 +0900

    refine linking scripts

--- shell/starship/link-linux.sh
+++ shell/starship/link-linux.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -f $HOME/.config/starship.toml ]; then
+    rm $HOME/.config/starship.toml
+fi
+
 mkdir -p $HOME/.config
 
 currentDirectory=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
--- shell/starship/link-pwsh.ps1
+++ shell/starship/link-pwsh.ps1
@@ -1,3 +1,10 @@
-New-Item -ItemType Directory -Path $env:USERPROFILE\.config 
 
-New-Item -ItemType HardLink -Path $env:USERPROFILE\.config\starship.toml -Target .\starship.toml
+if (-Not (Test-Path "$env:USERPROFILE\.config")) {
+	New-Item -ItemType Directory -Path $env:USERPROFILE\.config
+}
+
+if (Test-Path "$env:USERPROFILE\.config\starship.toml") {
+	Remove-Item "$env:USERPROFILE\.config\starship.toml"
+}
+
+New-Item -ItemType HardLink -Path "$env:USERPROFILE\.config\starship.toml" -Target ".\starship.toml"
