c08687ac1be02a10e1df5f8eb69a0a64e872fac7
excute <oys1751@gmail.com>
2024-03-13 11:19:14 +0900

    edit bash starship link script

--- shell/starship/link-linux.sh
+++ shell/starship/link-linux.sh
@@ -8,5 +8,4 @@ mkdir -p $HOME/.config
 
 currentDirectory=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-rm $HOME/.config/starship.toml
 ln -s ${currentDirectory}/starship.toml $HOME/.config/starship.toml
